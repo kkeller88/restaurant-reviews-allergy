1d182d5c7881cf9834d1a603f23ff220d845a4b0
Kristen Keller <kristenkeller@Kristens-MacBook-Pro.local>
2020-09-06 12:39:23 -0700

    add logging to annotate_sentences script

--- scripts/annotate_data/03_annotate_sentences.py
+++ scripts/annotate_data/03_annotate_sentences.py
@@ -5,12 +5,14 @@ import fire
 from restaurant_reviews_allergy.utils.mlflow import MlflowArtifactLogger, download_data
 from restaurant_reviews_allergy.review_parser.allergen_search import simple_allergen_search
 
+DEFAULT_ALLERGENS = ['allergy', 'celiac', 'intolerant', 'intolerance', 'dairy', 'egg', 'gluten', 'soy', 'peanut', ' nut', 'shellfish', 'wheat', 'seafood']
 
-def main(run_id, model_run_id, allergens=['gluten', 'soy', 'nut', 'a', 'the']):
+def main(run_id, model_run_id, allergens=DEFAULT_ALLERGENS):
     mlflow.set_experiment('restaurant-reviews-allergy')
     model_path = f'runs:/{model_run_id}/model'
     sentiment_model = load_model(model_path)
     sentences = download_data(run_id, 'sentences.pkl')
+    sentences['sentences'] = sentences['sentences'].str.lower()
     for allergen in allergens:
         sentences['is_' + allergen] = [
             1 if simple_allergen_search(x, allergen)
@@ -18,7 +20,12 @@ def main(run_id, model_run_id, allergens=['gluten', 'soy', 'nut', 'a', 'the']):
             for x in sentences['sentences']
             ]
     sentences = sentiment_model.predict(sentences)
-    print(sentences)
+    mlflow.log_param('run_id', run_id)
+    mlflow.log_param('model_run_id', model_run_id)
+    mlflow.set_tag('step', 'annotate_data')
+    logger = MlflowArtifactLogger()
+    logger.add_artifact(sentences, 'annotated_data.pkl')
+    logger.log_artifacts('')
 
 
 
